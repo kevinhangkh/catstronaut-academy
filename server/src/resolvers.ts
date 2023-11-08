9854719808880dd4021885052adf2cd7211059bb
Kevin Hang <kevinhang.kh@gmail.com>
2023-11-08 16:42:19 +0100

    Generate type with Codegen and specify types for Track and Author in models.ts

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,17 +1,18 @@
-const resolvers = {
+import { Resolvers } from './types';
+
+const resolvers: Resolvers = {
   Query: {
     // Returns an array of Tracks
     // that will be used to populate the homepage grid of our web client
     // (parent, args, contextValue, info)
-    tracksForHome: (_, __, contextValue: { dataSources: any }) => {
-      const { dataSources } = contextValue;
+    tracksForHome: (_, __, { dataSources }) => {
       return dataSources.trackAPI.getTracksForHome();
     },
   },
   Track: {
     // Here parent is equal to a Track object
-    author: (parent: { authorId: string }, _, { dataSources }) => {
-      const { authorId } = parent;
+    // and we're getting the authorId from it
+    author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
     },
   },
